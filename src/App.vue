8d574222db9840b53d00f3494d2307105211b26c
PaulRomanov <pavel.ramanau@gmail.com>
2023-09-21 13:29:04 +0400

    start project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <InventoryComponent />  
+</template>
+
+<script>
+import InventoryComponent from './components/InventoryComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InventoryComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/InventoryComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>InventoryComponent</div>
+</template>
+
+<script>
+export default {
+  name: "InventoryComponent",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
